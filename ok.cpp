f0ac06819df5aaa1c76e9154e8cc79c771132ce3
maikel112233 <maikel112233@gmail.com>
2017-08-06 10:59:40 +0200

    Aqui empezamos a conectar los reles

--- ok.cpp
+++ ok.cpp
@@ -1,19 +1,32 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include "RTClib.h"
-//#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-//esto es de que se esta haciendo una prueba
-RTC_DS1307 rtc;
-LiquidCrystal_I2C lcd(0x3f,16,2);
 
+//configuración de el relog
+RTC_DS1307 rtc;
 char daysOfTheWeek[7][12] = {"Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"};
+
+//configuración de la pantalla
+LiquidCrystal_I2C lcd(0x3f,16,2);
 char texto[10];
+
+//variables de reles
+ int pin[]={
+		  5,6,7,8,9,10,11,12
+ };
+ int pinTotal=8;
+
 void setup () {
   lcd.init();
   lcd.backlight();
   //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-
+  for (int var = 0; var < pinTotal ; ++var) {
+	pinMode(pin[var],OUTPUT);
+}
+  for (int var = 0; var < pinTotal; ++var) {
+	digitalWrite(pin[var],LOW);
+}
 }
 
 void loop () {
