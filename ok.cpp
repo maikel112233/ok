509f68f886019d0b1791ed56bc130cf7d7c29b49
maikel112233 <maikel112233@gmail.com>
2017-08-07 18:39:46 +0200

    asta aqui bien

--- ok.cpp
+++ ok.cpp
@@ -1,64 +1,77 @@
-#include <Arduino.h>
 #include <Wire.h>
 #include "RTClib.h"
-#include <LiquidCrystal_I2C.h>
-
-//configuraciÃ³n de el relog
-RTC_DS1307 rtc;
-char daysOfTheWeek[7][12] = {"DOMINGO", "LUNES", "MARTES", "MIERCOLES", "JUEVES", "VIERNES", "SABADO"};
-
-//configuraciÃ³n de la pantalla
-LiquidCrystal_I2C lcd(0x3f,16,2);
-char texto[10];
-
-//variables de reles
- int pin[]={
-		  5,6,7,8,9,10,11,12
- };
- int pinTotal=8;
- int horas=21;
- int minutos=4;
+RTC_DS1307 RTC;
+DateTime future = 0; // Variable para calcular cuando se debe desactivar la alarma
+// CONFIGURACIÓN DE USUARIO
+int seconds_alarm = 0; // Segundos alarma
+int minutes_alarm =40; // Minutos alarma
+int hour_alarm = 9; // Horas alarma. Formato 24h
+int day_alarm = 7; // Dia alarma
+int month_alarm = 8; // Mes alarma
+int year_alarm = 2017; // Año alarma
+int weekday_alarm = 1; // Dia de la semana alarma. L(1), M(2), MX(3), J(4), V(5), S(6), D(7)
+int alarm_duration = 10; // Tiempo activo de la alarma. Debe ser menor que la próxima alarma.
+boolean all_seconds = false; // Alarma para todos los segundos
+boolean all_minutes = false; // Alarma para todos los minutos
+boolean all_hour = true; // Alarma para todas las horas
+boolean all_weekday = true; // Alarma para todos los dias de la semana
+boolean all_day = true; // Alarma para todos los dias
+boolean all_month = true; // Alarma para todos los meses
+boolean all_year = true; // Alarma para todos los años
+int LedPin = 12;
+// -------------
 void setup () {
-  lcd.init();
-  lcd.backlight();
-  //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  for (int var = 0; var < pinTotal ; ++var) {
-	pinMode(pin[var],OUTPUT);
+Wire.begin(); // Inicia el puerto I2C
+RTC.begin(); // Inicia la comunicación con el RTC
+//RTC.adjust(DateTime(__DATE__, __TIME__)); // Establece la fecha y hora (Comentar una vez establecida la hora)
+pinMode(LedPin, OUTPUT);  // Establece el pin LedPin como salida
+digitalWrite(LedPin,HIGH);
+Serial.begin(9600); // Establece la velocidad de datos del puerto serie
 }
-  for (int var = 0; var < pinTotal; ++var) {
-	digitalWrite(pin[var],HIGH);
+void loop(){
+
+DateTime now = RTC.now(); // Obtiene la fecha y hora del RTC
+Serial.print(now.year(), DEC); // Año
+Serial.print('/');
+Serial.print(now.month(), DEC); // Mes
+Serial.print('/');
+Serial.print(now.day(), DEC); // Dia
+Serial.print(' ');
+Serial.print(now.hour(), DEC); // Horas
+Serial.print(':');
+Serial.print(now.minute(), DEC); // Minutos
+Serial.print(':');
+Serial.print(now.second(), DEC); // Segundos
+Serial.println();
+if (year_alarm == now.year() || all_year ) {
+  if (month_alarm == now.month() || all_month) {
+    if (day_alarm == now.day() || all_day) {
+      if (weekday_alarm == now.dayOfTheWeek()|| all_weekday) {
+        if (hour_alarm == now.hour() || all_hour) {
+          if (minutes_alarm == now.minute() || all_minutes) {
+            if (seconds_alarm == now.second() || all_seconds) {
+            	digitalWrite(LedPin,LOW);
+            future = now.unixtime() + alarm_duration;
+            Serial.println("------------");
+            Serial.println("ALARMA");
+            Serial.println("------------");
+            }
+          }
+        }
+      }
+    }
+  }
 }
+Serial.println(now.unixtime());
+Serial.println(future.unixtime());
+Serial.println(digitalRead(LedPin));
+
+if (now.unixtime() >= future.unixtime()){
+digitalWrite(LedPin, HIGH);
+Serial.println("------------");
+Serial.println("RESET ALARMA");
+Serial.println("------------");
+
 }
-
-void loop () {
-    DateTime now = rtc.now();
-    lcd.setCursor(0,0);
-    lcd.print(now.year(), DEC);
-    lcd.print('/');
-    lcd.print(now.month(), DEC);
-    lcd.print('/');
-    lcd.print(now.day(), DEC);
-    lcd.print(" ");
-    lcd.print(daysOfTheWeek[now.dayOfTheWeek()]);
-   //lcd.print(") ");
-   lcd.setCursor(0,1);
-   sprintf(texto,"%02d:%02d:%02d",now.hour(),now.minute(),now.second());
-   lcd.print(texto);
-
-   if(now.hour()==horas && now.minute()==minutos){
-	   digitalWrite(pin[0],LOW);
-	   lcd.setCursor(0,0);
-	   lcd.print("JARDINERAS UNO  ");
-   }else if(now.hour()==horas && now.minute()==minutos+1){
-	   digitalWrite(pin[0],HIGH);
-	   digitalWrite(pin[1],LOW);
-	   lcd.setCursor(0,0);
-	   lcd.print("JARDINERAS DOS  ");
-   }else if(now.hour()==horas && now.minute()==minutos+2){
-	   digitalWrite(pin[1],HIGH);
-   }
-
-
-
-    delay(1000);
+delay(1000); // La información se actualiza cada 1 seg.
 }
